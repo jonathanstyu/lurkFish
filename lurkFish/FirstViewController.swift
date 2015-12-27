038c9934b607a7d80d4b64b364a995a8b9d6c835
Jonathan Yu <jonathan@jonathanyu.net>
2015-12-27 10:40:09 -0800

    Initial Commit

---
+++ lurkFish/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  lurkFish
+//
+//  Created by Jonathan Yu on 12/27/15.
+//  Copyright © 2015 Jonathan Yu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
